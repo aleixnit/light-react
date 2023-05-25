94323fa026426c6f3cfc1c1777ca052b7f10294c
aleixnit <nitjac210@gmail.com>
2023-05-25 14:22:32 +0200

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
-import './App.css'
-import LightOnImg from './assets/lighton.png'
-import LightOffImg from './assets/lightoff.png'
+import "./App.css";
+import LightOnImg from "./assets/lighton.png";
+import LightOffImg from "./assets/lightoff.png";
+import React, { useState } from "react";
 
 function App() {
-
   return (
     <>
       <Light />
     </>
-  )
+  );
 }
 
 function Light() {
+  const [statusLight, setStatusLight] = useState("on");
 
-  return <div>
-    <h1>Click the buttons to turn on or off the light</h1>
-    <img src={LightOnImg} />
-    <div><button>Turn On</button><button>Turn Off</button></div>
-  </div>
+  function apagarEncender() {
+  if (statusLight === 'on') {
+    setStatusLight('off');
+  } else if (statusLight === 'off') {
+    setStatusLight('on');
+  }
 }
 
-export default App
+  return (
+    <div>
+      <h1>Click the buttons to turn on or off the light</h1>
+      <img src={statusLight === "on" ? LightOnImg : LightOffImg} />
+      <div>
+        <button
+          style={{ display: statusLight === "on" ? "none" : "block" }}
+          onClick={apagarEncender}
+        >
+          Turn On
+        </button>
+        <button
+          onClick={apagarEncender}
+          style={{ display: statusLight === "off" ? "none" : "block" }}
+        >
+          Turn Off
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
